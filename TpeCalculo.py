74879a8cf4cc0e9392db09b0231dc19a97d2f9ad
unknown <matiassorenson@gmail.com>
2022-06-20 19:33:53 -0300

    Correccion de valores iniciales en ecua dif mas prints

--- TpeCalculo.py
+++ TpeCalculo.py
@@ -1,18 +1,33 @@
 import numpy as np
 
-yi=0
-xi=0
-xf=1
-N=10
-h=(xf-xi)/N
-y=np.zeros(shape=N+1)
-
 def f(x,y):
     I = 10
     F = I
     C = 0.2
-    V = 200
+    V = 500
     return (C * I - F/V * y)
+
+
+yi=0
+xi=0
+xf=20
+N=20000
+h=(xf-xi)/N
+y=np.zeros(shape=N+1)
+print(
+f'''Condiciones iniciales:
+    Concentracion inicial = {yi}
+    Tiempo final = {xf}
+    longitudes para aprox = {h}
+    Flujo de Entrada = 10
+    Flujo de Salida = 10
+    Concentracion del Flujo de entrada = 0.2
+    Volumen Total = 500
+
+    ''')
+
+print(f"h = {h}")
+print("Metodo de Euler")
 
 
 def rungeKutta(h, x0, y0):
@@ -31,30 +46,30 @@ def rungeKutta(h, x0, y0):
 ##Euler
 x=xi
 y[0] = yi
-print(f"En {x}, y = {y[0]}")
+print(f"En {xi} y = {y[0]}")
 for i in range(1,N+1):
     y[i] = y[i-1] + h * f(x,y[i-1])
     x += h
-print(f"En {x}, y = {y[i]}")
+print(f"En {xf} y = {y[i]}")
 
 
 ##Euler Mejorado
-print("EULER Mejorado")
+print("Euler Mejorado")
 x=xi
 y[0] = yi
-print(f"En {x}, y = {y[0]}")
+print(f"En {xi} y = {y[0]}")
 for i in range(1,N+1):
     yaux = y[i-1] + h * f(x,y[i-1])
     y[i] = y[i-1] + h/2 * (f(x,y[i-1]) + f(x+h,yaux))
     x += h
-print(f"En {x}, y = {y[i]}")
+print(f"En {xf} y = {y[i]}")
 
 ## rungeKutta
 print("rungeKutta")
 x=xi
 y[0] = yi
-print(f"En {x}, y = {y[0]}")
+print(f"En {xi} y = {y[0]}")
 for i in range(1,N+1):
     y[i] = rungeKutta(h,x,y[i-1])
     x+=h
-print(f"En {x}, y = {y[i]}")
+print(f"En {xf} y = {y[i]}")
